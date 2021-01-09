936593678e1d1b44900866dfaaa8d0312a15f376
Daren McCarrellgit config --global user.email darenmcc16@gmail.com <darenmcc16@gmail.com>
2021-01-08 20:06:56 -0600

    final commit

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React from 'react'
 
-function Search(props){
-    return(
-        <section className="search">
-            <form>
-            <label>
-                <span>Search books</span>
-                <input
-                type="search"
-                placeholder="henry"
-                />
-            </label>
-            </form>
-        </section>
-    )
+class Search extends React.Component {
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.props.handleSearch}>
+                    <label>Search:</label>
+                    <input type='text'
+                    name='q' 
+                    className='search-bar' 
+                    placeholder='Crit Racing' 
+                    required />
+                    <button type="submit">search</button>
+                    <div className='filter-bar'>
+                        <label>Print Type</label>
+                        <select name="printType">
+                            <option value='all'>All</option>
+                            <option value='books'>Books</option>
+                            <option value='magazines'>magazines</option>
+                        </select>
+                        <label>Book Type</label>
+                        <select name='filter'>
+                            <option value='ebooks'>ebooks</option>
+                            <option value='free-ebooks'>Free-ebooks</option>
+                            <option value='full'>Full</option>
+                            <option value='paid-ebooks'>Paid</option>
+                            <option value='partial'>previews</option>
+                        </select>
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
 }
 
-export default Search;
+export default Search
